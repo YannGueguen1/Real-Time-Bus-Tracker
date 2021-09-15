881476c566f255884e1d7a02e24ba786b4195d9c
YannGueguen1 <yanngueguen2015@u.northwestern.edu>
2021-09-14 23:51:37 -0500

    personalize markers

--- mapanimation.js
+++ mapanimation.js
@@ -15,18 +15,19 @@ const busStops = [
 ];
 
 // TODO: add your own access token
-mapboxgl.accessToken = 'pk.eyJ1IjoieWFubmd1ZWd1ZW4iLCJhIjoiY2tzZ3JwZDhyMW5nYTMwb21peGYzMzR6eiJ9.Q4dRQr58ib-_kYIQ5AhaPg';
+mapboxgl.accessToken =
+  "pk.eyJ1IjoieWFubmd1ZWd1ZW4iLCJhIjoiY2tzZ3JwZDhyMW5nYTMwb21peGYzMzR6eiJ9.Q4dRQr58ib-_kYIQ5AhaPg";
 
 // This is the map instance
 let map = new mapboxgl.Map({
-  container: 'map',
-  style: 'mapbox://styles/mapbox/streets-v11',
+  container: "map",
+  style: "mapbox://styles/mapbox/streets-v11",
   center: [-71.104081, 42.365554],
   zoom: 14,
 });
 
 // TODO: add a marker to the map at the first coordinates in the array busStops. The marker variable should be named "marker"
-const marker = new mapboxgl.Marker()
+const marker = new mapboxgl.Marker({ color: "#b40219" })
   .setLngLat(busStops[0])
   .addTo(map);
 
@@ -34,17 +35,89 @@ const marker = new mapboxgl.Marker()
 let counter = 0;
 function move() {
   // TODO: move the marker on the map every 1000ms. Use the function marker.setLngLat() to update the marker coordinates
-  setTimeout(() =>{
+  setTimeout(() => {
+    if (counter >= busStops.length) return;
     marker.setLngLat(busStops[counter]);
+    map.flyTo({
+      center: busStops[counter],
+    });
     counter++;
-    move()
-  },1000)
-  // Use counter to access bus stops in the array busStops
-  // Make sure you call move() after you increment the counter.
+    move();
+  }, 1000);
 }
-move()
+
+const places = {
+  type: "FeatureCollection",
+  features: [
+    {
+      type: "University",
+      properties: {
+        id: "startMarker",
+        description: "MIT",
+        title: "Start",
+      },
+      geometry: {
+        type: "Point",
+        coordinates: [-71.093729, 42.359244],
+      },
+    },
+    {
+      type: "Feature",
+      properties: {
+        id: "finishMarker",
+        title: "Finish",
+        description: "Harvard",
+      },
+      geometry: {
+        type: "Point",
+        coordinates: [-71.118625, 42.374863],
+      },
+    },
+  ],
+};
+
+map.on("load", () => {
+  // Add a GeoJSON source containing place coordinates and information.
+  map.addSource("places", {
+    type: "geojson",
+    data: places,
+  });
+
+  map.addLayer({
+    id: "poi-labels",
+    type: "symbol",
+    source: "places",
+    layout: {
+      "text-field": ["get", "description"],
+      "text-variable-anchor": ["top", "bottom", "left", "right"],
+      "text-radial-offset": 0.5,
+      "text-justify": "auto",
+      "icon-image": ["get", "icon"],
+    },
+  });
+
+  // map.rotateTo(180, { duration: 10000 });
+});
+
+// add markers to map
+for (const { geometry, properties } of places.features) {
+  // create a HTML element for each feature
+  const el = document.createElement("div");
+  el.className = "marker";
+  // el.style.backgroundImage = "url('startIcon.png')";
+  el.setAttribute('id',properties.id);
+
+  // make a marker for each feature and add it to the map
+  new mapboxgl.Marker(el)
+    .setLngLat(geometry.coordinates)
+    .setPopup(
+      new mapboxgl.Popup({ offset: 25 }) // add popups
+        .setHTML(`<h3>${properties.title}</h3><p>${properties.description}</p>`)
+    )
+    .addTo(map);
+}
 
 // Do not edit code past this point
-if (typeof module !== 'undefined') {
+if (typeof module !== "undefined") {
   module.exports = { move };
 }
